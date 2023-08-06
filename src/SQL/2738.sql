b91263d364490583afb1ea9d152e81a94b0564c5
David Wesley <contato.dwesley@outlook.com>
2023-08-06 03:13:37 -0300

    feat: 2738 - Contest

---
+++ src/SQL/2738.sql
@@ -0,0 +1,13 @@
+SELECT
+  candidate.name,
+  ROUND(
+    (
+      2 * score.math + 3 * score.specific + 5 * score.project_plan
+    ) / 10.0,
+    2
+  ) AS "avg"
+FROM
+  candidate
+  INNER JOIN score ON candidate.id = score.candidate_id
+ORDER BY
+  "avg" DESC;
